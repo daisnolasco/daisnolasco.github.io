561c077bce8059de41b0986c9c5aae7c90a25f42
daisnolasco <nolascd2@wwu.edu>
2024-11-05 22:38:25 -0800

    Add files via upload

---
+++ jsonHomes/house.js
@@ -0,0 +1,42 @@
+
+fetch('houseData.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('architecturetable');
+
+    data.forEach(item => {
+     
+      const card = document.createElement('div');
+      card.className = 'card';
+
+
+      const title = document.createElement('h2');
+      title.textContent = item.title;
+
+
+      const img = document.createElement('img');
+      img.src = item.imageUrl;
+      img.alt = item.altText;
+
+
+      const description = document.createElement('p');
+      description.textContent = item.text;
+
+   
+      const link = document.createElement('a');
+      link.href = item.WebsiteUrl;
+      link.target = "_blank";
+      link.textContent = "Learn more";
+
+    
+      card.appendChild(title);
+      card.appendChild(img);
+      card.appendChild(description);
+      card.appendChild(link);
+
+   
+      container.appendChild(card);
+    });
+  })
+  .catch(error => console.error('Error loading JSON:', error));
+  
